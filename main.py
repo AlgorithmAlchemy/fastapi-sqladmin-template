523aa3c4e8900475cf0b89b3a78f3b05097a61c4
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-13 05:06:32 +0300

    Commit

--- main.py
+++ main.py
@@ -2,16 +2,28 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from admin import admin_app
+import models
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
 
 app = FastAPI()
 
-# Подключаем папку со статикой
+# Static и Templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-# Подключаем шаблоны
 templates = Jinja2Templates(directory="templates")
 
-# Роут на главную страницу
+# Admin
+app.mount("/admin", admin_app)
+
+# Роут на главную
 @app.get("/", response_class=HTMLResponse)
 async def read_index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# Создание таблиц при старте
+@app.on_event("startup")
+async def startup_event():
+    engine = create_async_engine("sqlite+aiosqlite:///./test.db", echo=True)
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
