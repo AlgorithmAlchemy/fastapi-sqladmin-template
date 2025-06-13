0bae5f44952e7c179d32221738e47e539bc75276
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-13 05:32:32 +0300

    Commit

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from models import Base, Product
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-engine = create_async_engine(DATABASE_URL)
+engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 app = FastAPI()
