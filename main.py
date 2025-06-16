12beb834db878879bf1b25e84d09a41388731b90
AlgorithmAlchemy <darkydark@cyberfear.com>
2025-06-17 00:38:08 +0300

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
@@ -9,13 +11,16 @@ DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Здесь выполняется код при запуске
+    print("Startup actions")
+    yield
+    # Здесь выполняется код при завершении
+    print("Shutdown actions")
 
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
+
+app = FastAPI(lifespan=lifespan)
 
 admin = Admin(app, engine)
 admin.add_view(ProductAdmin)
